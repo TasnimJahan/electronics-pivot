adba48215e322f69953c9e40cbcae55d28fc171c
Sultana Tasnim Jahan <tasnim047sultana@gmail.com>
2021-04-17 20:31:36 +0600

    dynamic reviews complete

--- src/components/Admin/Sidebar/Sidebar.js
+++ src/components/Admin/Sidebar/Sidebar.js
@@ -8,73 +8,34 @@ import { UserContext } from '../../../App';
 
 const Sidebar = () => {
     const [loggedInUser, setLoggedInUser]= useContext(UserContext);
-    // const [isDoctor,setIsDoctor] = useState(false);
+    const [isAdmin,setIsAdmin] = useState(false);
 
-    // useEffect(() => {
-    //     fetch('https://rocky-forest-80852.herokuapp.com/isDoctor', {
-    //         method: 'POST',
-    //         headers: {'Content-Type': 'application/json'},
-    //         body: JSON.stringify({email: loggedInUser.email})
-    //     })
-    //     .then(res => res.json())
-    //     .then(data => setIsDoctor(data));
-    // },[])
+    useEffect(() => {
+        fetch('http://localhost:5000/isAdmin', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({email: loggedInUser.email})
+        })
+        .then(res => res.json())
+        .then(data => setIsAdmin(data));
+    },[])
 
     return (
         <div className="sidebar d-flex flex-column justify-content-between col-md-2 py-5 px-4" style={{height:"100vh"}}>
             <ul className="list-unstyled">
-                {/* {
-                    isDoctor && <li>
-                        <Link to="/dashboard" className="text-white">
-                            <FontAwesomeIcon className="icons" icon={faGripHorizontal} /> <span style={{marginLeft:'17%'}}>Dashboard</span> 
-                        </Link>
-                    </li>
-                }
-                <li>
-                    <Link to="/dashboard/appointment" className="text-white">
-                        <FontAwesomeIcon className="icons" icon={faCalendar} /> <span style={{marginLeft:'17%'}}>Appointment</span> 
-                    </Link>
-                </li>
                 {
-                    isDoctor && <div>
+                    isAdmin && <div>
                         <li>
-                            <Link to="/dashboard/allPatients" className="text-white">
-                                <FontAwesomeIcon className="icons" icon={faUsers} /> <span>Patients</span>
+                            <Link to="/dashboard" className="text-white">
+                                <FontAwesomeIcon className="icons" icon={faGripHorizontal} /> <span style={{marginLeft:'10%'}}>Order List</span> 
                             </Link>
                         </li>
+                        
                         <li>
-                            <Link to="/dashboard/prescriptions" className="text-white">
-                                <FontAwesomeIcon className="icons" icon={faFileAlt} /> <span style={{marginLeft:'6%'}}>Prescriptions</span>
+                            <Link to="/addServices" className="text-white">
+                                <FontAwesomeIcon className="icons" icon={faPlus} /> <span style={{marginLeft:'10%'}}>Add Service</span> 
                             </Link>
                         </li>
-                        <li>
-                            <Link to="/dashboard/addDoctor" className="text-white">
-                                <FontAwesomeIcon className="icons" icon={faUserPlus} /> <span style={{marginLeft:'0%'}}>Add Doctor</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/doctor/setting" className="text-white" >
-                            <FontAwesomeIcon className="icons" icon={faCog} /> <span style={{marginLeft:'4%'}}>Setting</span>
-                            </Link>
-                        </li>
-                    </div>
-                } */}
-
-
-
-                    <li>
-                        <Link to="/dashboard" className="text-white">
-                            <FontAwesomeIcon className="icons" icon={faGripHorizontal} /> <span style={{marginLeft:'10%'}}>Order List</span> 
-                        </Link>
-                    </li>
-                
-                <li>
-                    <Link to="/addServices" className="text-white">
-                        <FontAwesomeIcon className="icons" icon={faPlus} /> <span style={{marginLeft:'10%'}}>Add Service</span> 
-                    </Link>
-                </li>
-                
-                    <div>
                         <li>
                             <Link to="/makeAdmin" className="text-white">
                                 <FontAwesomeIcon className="icons" icon={faUserShield} /> <span  style={{marginLeft:'7%'}}>Make Admin</span>
@@ -85,13 +46,16 @@ const Sidebar = () => {
                                 <FontAwesomeIcon className="icons" icon={faFileAlt} /> <span style={{marginLeft:'12%'}}>Manage Service</span>
                             </Link>
                         </li>
+                    </div>
+                }
+                        
                         <li>
-                            <Link to="/dashboard/addDoctor" className="text-white">
+                            <Link to="/book" className="text-white">
                                 <FontAwesomeIcon className="icons" icon={faShoppingCart} /> <span style={{marginLeft:'9%'}}>Book</span>
                             </Link>
                         </li>
                         <li>
-                            <Link to="/doctor/setting" className="text-white" >
+                            <Link to="/myBookings" className="text-white" >
                             <FontAwesomeIcon className="icons" icon={faLock} /> <span style={{marginLeft:'11%'}}>Booking List</span>
                             </Link>
                         </li>
@@ -100,7 +64,7 @@ const Sidebar = () => {
                             <FontAwesomeIcon className="icons" icon={faComments} /> <span style={{marginLeft:'9%'}}>Review</span>
                             </Link>
                         </li>
-                    </div>
+                    
             
             </ul>
             <div>
